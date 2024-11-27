fca1a99bec8ac480e8272309950ca1ca79a4aa67
OmowunmiKamil <balikiskamil@gmail.com>
2024-11-27 17:33:31 +0100

    Refactor asset imports to follow consistent naming conventions

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,18 +4,18 @@ import checkBold from "./check_bold.svg";
 import close from "./close_small.svg";
 import aboutLogo from "./moonex-about.png";
 import aboutBg from "./about-bg.png";
-import comparison from "./comparison.png";
-import dollar from "./dollar.svg";
+import comparison from "./Comparison.png";
+import dollar from "./Dollar.svg";
 import footerLogo from "./footer-logo.svg";
 import foreground from "./foreground.png";
-import plus from "./minus_L.svg";
-import options from "./options_3.svg";
-import phone from "./phone_off.svg";
-import shield from "./shield_Check.svg";
-import telegram from "./social_icons.png";
+import plus from "./Minus_L.svg";
+import options from "./Options_3.svg";
+import phone from "./Phone_off.svg";
+import shield from "./Shield_Check.svg";
+import telegram from "./Social_icons.png";
 import twitter from "./Twitter.svg";
-import uniswap from "./uniswap.png";
-import vector from "./vector.svg";
+import uniswap from "./Uniswap.png";
+import vector from "./Vector.svg";
 
 export const asserts = {
   bg,
